42a9f965e7dc0192fecc069ade671215f4344cd5
loic <loic.jacquel.fr@gmail.com>
2020-11-03 21:00:07 +0900

    add post route

--- public/client.js
+++ public/client.js
@@ -10,6 +10,29 @@ const santaForm = document.forms[0];
 
 // listen for the form to be submitted and add a new dream when it is
 santaForm.onsubmit = function (event) {
-  // TODO: check the text isn't more than 100chars before submitting
-  // event.preventDefault();
+  var errors = [];
+
+  // check user id
+  if(document.getElementById("userid").value.length > 30) {
+    errors.push("Your wish can not be more that 100 characters !");
+  }
+
+  if(document.getElementById("userid").value.length == 0) {
+    errors.push("Please enter your user id");
+  }
+
+  // check wish
+  if(document.getElementById("wish").value.length > 100) {
+    errors.push("Your wish can not be more that 100 characters !");
+  }
+
+  if(document.getElementById("wish").value.length == 0) {
+    errors.push("Please enter a wish");
+  }
+
+  if(errors.length > 0) {
+    alert(errors.join("\n"));
+    event.preventDefault();
+  }
+
 };
--- routes/santa.js
+++ routes/santa.js
@@ -1,12 +1,26 @@
 const express = require('express');
+const { check, validationResult } = require('express-validator');
 const router = express.Router();
 
+
 router.get('/', (request, response) => {
-    response.render('index');
+    response.render('index', { userid:"", wish:"", error:"" });
 });
 
-router.post('/', (request, response) => {
-    response.render('index');
+router.post('/', [
+    check('userid', 'The name is required').exists().isLength( { min:1 }),
+    check('userid', 'The name must be less than 30 characters').isLength( { max:30 }),
+    check('wish', 'The name is required').exists().isLength( { min:1 }),
+    check('wish', 'The name must be less than 100 characters').isLength( { max:100 }),
+], (request, response) => {
+    const { wish, userid } = { ...request.body };
+    
+    const errors = validationResult(request);
+    if(!errors.isEmpty()) {
+        return response.status(400).send(errors.array().map(e => e.msg).join(','));
+    }
+
+    response.render('index', { userid, wish, error:"" });
 });
 
 module.exports = router;
